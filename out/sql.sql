061777ab0beb44633f8ab3a269ec7d2cc285c70b
thumb0422 <thumb0422@163.com>
2020-12-15 19:13:25 +0800

    update
    add sqlscript

---
+++ out/sql.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS "T_M_Datas" (
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"MMac"	varchar(50) NOT NULL,
+	"MSBP"	varchar(20),
+	"MDBP"	varchar(20),
+	"MHR"	varchar(20),
+	"MDate"	datetime DEFAULT (datetime(CURRENT_TIMESTAMP,'localtime'))
+);
+CREATE TABLE IF NOT EXISTS "T_M_User" (
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"MMac"	varchar(50) NOT NULL,
+	"MUser"	varchar(50),
+	"MDesc"	varchar(250)
+);
+CREATE TABLE IF NOT EXISTS "T_M_Infos" (
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"MNo"	varchar(50) NOT NULL,
+	"MMac"	varchar(50) NOT NULL,
+	"MGroup" varchar(10),
+	"MDesc"	varchar(250)
+);
